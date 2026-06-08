3ff3ac65aa73d103014b27ccfb8fad956ffc3ecf
alex_blynduk <alexblynduk05@gmail.com>
2026-06-08 15:57:37 +0300

    Added read function

--- Core/Src/Device_drivers/nvm_high_api.c
+++ Core/Src/Device_drivers/nvm_high_api.c
@@ -3,11 +3,31 @@
 
 extern nvm_device_api_t api_low;
 
+nvm_data_cache data_cache;
+
 static nvm_high_api_status_t init  (nvm_device_api_handle *const wl_handle, nvm_formatting_status_t format){
 	 return api_low.init(wl_handle);
 }
+
+
 static nvm_high_api_status_t read  (nvm_device_api_handle *const wl_handle, nvm_data_t *data){
-	return api_low.read(wl_handle, &(data->data), sizeof(data->data));
+	if (0 == wl_handle->is_device_initialized) {
+		return NVM_API_STATUS_NOT_INITIALIZED;
+	}
+
+	if (NULL == wl_handle || NULL == wl_handle->hi2c || 0 == wl_handle->device_address || NULL == data) {
+		return NVM_API_STATUS_INVALID_PARAMETERS;
+	}
+
+	if (1 == data_cache.is_valid) {
+		*data = data_cache.data;
+	} else {
+		if (api_low.read(wl_handle, &(data->data), sizeof(data->data)) != NVM_DEVICE_STATUS_OK ) {
+			return NVM_API_STATUS_READ_ERROR;
+		}
+	}
+
+	return NVM_API_STATUS_OK;
 }
 static nvm_high_api_status_t write (nvm_device_api_handle *const wl_handle, const nvm_data_t *const data){
 	return NVM_API_STATUS_OK;
