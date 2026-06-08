a95e94a88f54f84bd5f6266c3c427a8ba38a45e5
illia-kazantsev <kazantsevid@gmail.com>
2026-06-08 16:07:37 +0300

    Search & Read

--- Core/Inc/Device_drivers/at24c256n.h
+++ Core/Inc/Device_drivers/at24c256n.h
@@ -6,7 +6,7 @@ typedef struct
 {
 	nvm_device_status_t (*init)  (nvm_device_api_handle *const wl_handle);
 	nvm_device_status_t (*read)  (nvm_device_api_handle *const wl_handle, uint8_t *const data, const uint16_t size);
-	nvm_device_status_t (*search_last_busy_page) (nvm_device_api_handle *const wl_handle, const uint16_t size);
+	nvm_device_status_t (*search_last_busy_page) (nvm_device_api_handle *const wl_handle);
 	nvm_device_status_t (*write) (nvm_device_api_handle *const wl_handle, const uint8_t *const data, const uint16_t size);
 	nvm_device_status_t (*erase_all) (nvm_device_api_handle *const wl_handle);
 } nvm_device_api_t;
--- Core/Src/Device_drivers/at24c256n.c
+++ Core/Src/Device_drivers/at24c256n.c
@@ -8,7 +8,28 @@ static nvm_device_status_t init_low  		(nvm_device_api_handle *const wl_handle){
 	return NVM_DEVICE_STATUS_OK; //!
 }
 static nvm_device_status_t read_low  		(nvm_device_api_handle *const wl_handle, uint8_t *const data, const uint16_t size){
+
+	if (at24c256n_wait_for_ready(wl_handle) != HAL_OK)
+	{
+		return NVM_DEVICE_STATUS_NOT_CONNECTED;
+	}
+
+	uint16_t addr = wl_handle->last_busy_page * wl_handle->device_mem_page;
+
+	if (HAL_I2C_Mem_Read(
+		wl_handle->hi2c,
+	    wl_handle->device_address,
+	    addr,
+	    I2C_MEMADD_SIZE_16BIT,
+	    data,
+	    size,
+	    100) != HAL_OK)
+	{
+		return NVM_DEVICE_STATUS_READ_ERROR;
+	}
+
 	return NVM_DEVICE_STATUS_OK;
+
 }
 static nvm_device_status_t write_low 		(nvm_device_api_handle *const wl_handle, const uint8_t *const data, const uint16_t size){
 	return NVM_DEVICE_STATUS_OK; //!
@@ -16,7 +37,51 @@ static nvm_device_status_t write_low 		(nvm_device_api_handle *const wl_handle, 
 static nvm_device_status_t erase_all_low 	(nvm_device_api_handle *const wl_handle){
 	return NVM_DEVICE_STATUS_OK; //!
 }
-static nvm_device_status_t search_last_busy_page  	(nvm_device_api_handle *const wl_handle, const uint16_t size){
+static nvm_device_status_t search_last_busy_page  	(nvm_device_api_handle *const wl_handle){
+
+	if (at24c256n_wait_for_ready(wl_handle) != HAL_OK)
+	{
+		return NVM_DEVICE_STATUS_NOT_CONNECTED;
+	}
+
+    uint8_t buffer[wl_handle->device_mem_page];
+    uint16_t pages = wl_handle->device_mem_capacity / wl_handle->device_mem_page;
+
+    for (uint16_t page = 0; page < pages; page++)
+    {
+        uint16_t addr = page * wl_handle->device_mem_page;
+
+        if (HAL_I2C_Mem_Read(
+                wl_handle->hi2c,
+                wl_handle->device_address,
+                addr,
+                I2C_MEMADD_SIZE_16BIT,
+                buffer,
+                wl_handle->device_mem_page,
+                100) != HAL_OK)
+        {
+            return NVM_DEVICE_STATUS_READ_ERROR;
+        }
+
+        uint8_t empty = 1;
+
+        for (uint16_t i = 0; i < wl_handle->device_mem_page; i++)
+        {
+        	if (buffer[i] != 0xFF)
+        	{
+        		empty = 0;
+        		break;
+        	}
+        }
+
+        if (empty)
+        {
+        	wl_handle->last_busy_page = (page == 0) ? 0 : (page - 1);
+        	return NVM_DEVICE_STATUS_OK;
+        }
+    }
+    wl_handle->last_busy_page = pages - 1;
+
 	return NVM_DEVICE_STATUS_OK;
 }
 
