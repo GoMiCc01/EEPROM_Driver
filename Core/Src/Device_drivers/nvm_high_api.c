d67b2a4ab7d7ed2475ec57f1ec1151ac5a970a3d
Ihoryakib <igoryakib@gmail.com>
2026-06-08 14:14:57 +0300

    added write func

--- Core/Src/Device_drivers/nvm_high_api.c
+++ Core/Src/Device_drivers/nvm_high_api.c
@@ -1,7 +1,10 @@
 
 #include "nvm_high_api.h"
+#define LAST_BUSY_PAGE 0x7fff
 
 extern nvm_device_api_t api_low;
+
+nvm_data_cache data_cache;
 
 static nvm_high_api_status_t init  (nvm_device_api_handle *const wl_handle, nvm_formatting_status_t format){
 	 return api_low.init(wl_handle);
@@ -10,7 +13,26 @@ static nvm_high_api_status_t read  (nvm_device_api_handle *const wl_handle, nvm_
 	return api_low.read(wl_handle, &(data->data), sizeof(data->data));
 }
 static nvm_high_api_status_t write (nvm_device_api_handle *const wl_handle, const nvm_data_t *const data){
-	return NVM_API_STATUS_OK;
+
+	if (1 != wl_handle->is_device_initialized) {
+		return NVM_API_STATUS_NOT_INITIALIZED;
+	}
+
+	if (NULL == wl_handle->device_address || NULL == wl_handle->hi2c || NULL == data->data) {
+		return NVM_API_STATUS_INVALID_PARAMETERS;
+	}
+	if (LAST_BUSY_PAGE == wl_handle->last_busy_page) {
+		if (NVM_DEVICE_STATUS_OK != api_low.erase_all(wl_handle)) return NVM_API_STATUS_WRITE_ERROR;
+	}
+
+	if (NVM_DEVICE_STATUS_OK != api_low.write(wl_handle, data->data, sizeof(data->data))) {
+		return NVM_API_STATUS_WRITE_ERROR;
+	} else {
+		data_cache.data = data->data;
+		data_cache.is_valid = 1;
+		return NVM_API_STATUS_OK;
+	}
+
 }
 
 nvm_api_t api ={
